d5a8eee1fc1a7f9fa5959fcd5bf602cb64137be6
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-02-19 10:56:48 +0700

    add: index page with banner

--- course-management-system/src/App.vue
+++ course-management-system/src/App.vue
@@ -1,11 +1,27 @@
-<script setup></script>
+<script setup>
+import Header from "@/components/header.vue";
+import { HugeiconsIcon } from "@hugeicons/vue";
+import * as icons from "@hugeicons/core-free-icons"
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Header/>
+
+  <main>
+    <section class="h-screen flex  items-center justify-center flex-col gap-8">
+      <h1 class="text-5xl font-bold">Couse Management System</h1>
+      <p class="text-center px-16">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam repellendus rem, illum dolorum ipsa sed sunt asperiores expedita. Dignissimos vitae minima error provident hic blanditiis modi harum! Ex, sapiente.</p>
+      <button class="px-4 py-2 bg-ci-primary flex justify-center rounded-xl">
+        Get Started
+        <HugeiconsIcon
+        :icon="icons.ArrowRight01Icon"
+        :size="24"
+        color="currentColor"
+        :stroke-width="1.5"
+        />
+      </button>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ course-management-system/src/components/header.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { HugeiconsIcon } from '@hugeicons/vue';
+import { Moon02Icon } from '@hugeicons/core-free-icons';
+
+function changeTheme(){
+    document.body.classList = "bg-bg-dark-1 text-text-100"
+}
+</script>
+
+<template>
+    <header class="flex fixed w-screen justify-between px-8 py-4 items-center">
+        <h1 class="text-xl font-bold">CMS</h1>
+        <div class="flex gap-4 items-center">
+            <button class=" px-4 py-2 rounded-xl w-25 bg-ci-primary">Log in</button>
+            <button class="border border-slate-400 px-4 py-2 rounded-xl w-25">Sign in</button>
+            <button v-on:click="changeTheme()" class="border border-slate-400 p-2 rounded-xl">
+                <HugeiconsIcon
+                :icon="Moon02Icon"
+                :size="24"
+                color="currentColor"
+                :stroke-width="1.5"
+                />
+            </button>
+        </div>
+    </header>
+</template>
