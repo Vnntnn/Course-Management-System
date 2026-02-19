a95eb480cfe0cdd476428fe24d8d26abc2d2a973
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-02-19 11:23:35 +0700

    add: update banner

--- course-management-system/src/App.vue
+++ course-management-system/src/App.vue
@@ -9,17 +9,20 @@ import * as icons from "@hugeicons/core-free-icons"
 
   <main>
     <section class="h-screen flex  items-center justify-center flex-col gap-8">
-      <h1 class="text-5xl font-bold">Couse Management System</h1>
+      <h1 class="text-6xl font-bold">Course Management System</h1>
       <p class="text-center px-16">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam repellendus rem, illum dolorum ipsa sed sunt asperiores expedita. Dignissimos vitae minima error provident hic blanditiis modi harum! Ex, sapiente.</p>
-      <button class="px-4 py-2 bg-ci-primary flex justify-center rounded-xl">
+      <div class="flex flex-col gap-2 items-center">
+        <button class="px-4 py-2 w-50 h-12 items-center bg-ci-primary flex justify-between rounded-xl text-lg font-semibold hover:bg-ci-secondary-2 transition-all duration-200 text-text-100">
         Get Started
         <HugeiconsIcon
         :icon="icons.ArrowRight01Icon"
         :size="24"
         color="currentColor"
-        :stroke-width="1.5"
+        :stroke-width="2.5"
         />
-      </button>
+        </button>
+        <p class="text-text-400">Already have an Acccount? <a href="#" class="underline">Log In</a></p>
+      </div>
     </section>
   </main>
 </template>
--- course-management-system/src/components/header.vue
+++ course-management-system/src/components/header.vue
@@ -8,10 +8,10 @@ function changeTheme(){
 </script>
 
 <template>
-    <header class="flex fixed w-screen justify-between px-8 py-4 items-center">
+    <header class="flex fixed w-screen justify-between px-8 py-4 items-center shadow-lg">
         <h1 class="text-xl font-bold">CMS</h1>
         <div class="flex gap-4 items-center">
-            <button class=" px-4 py-2 rounded-xl w-25 bg-ci-primary">Log in</button>
+            <button class=" px-4 py-2 rounded-xl w-25 bg-ci-primary text-text-100">Log in</button>
             <button class="border border-slate-400 px-4 py-2 rounded-xl w-25">Sign in</button>
             <button v-on:click="changeTheme()" class="border border-slate-400 p-2 rounded-xl">
                 <HugeiconsIcon
