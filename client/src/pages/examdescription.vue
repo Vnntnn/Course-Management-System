85fd2867e7e09bf4c9a39bdd9867ad3e7ae5c27c
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-03-09 12:30:45 +0700

    a

---
+++ client/src/pages/examdescription.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <main class="mt-24 px-5">
+        <h1>Exam Description</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo similique omnis dolorum est amet maxime totam voluptate? Eveniet facere dolorem similique dignissimos repellendus libero, aut, possimus quia neque, accusantium officia.</p>
+    </main>
+</template>
